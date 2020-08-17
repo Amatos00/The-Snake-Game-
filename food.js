2bc99d27abdacb2ae9988fbed675320afaf9bed0
Anderson de Oliveira <amatos.oliveira2197@gmail.com>
2020-08-17 20:32:32 -0300

    First commit

---
+++ food.js
@@ -0,0 +1,29 @@
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 5
+
+export function update(){
+    if(onSnake(food)) {
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition()
+    }
+}
+
+export function draw(gameBoard) {
+    const foodElement = document.createElement('div')
+    snakeElement.style.gridRowStart = food.y
+    snakeElement.style.gridColumnStart = food.x
+    snakeElement.classList.add('food')
+    gameBoard.appendChild(foodElement)
+    
+}
+
+function getRandomFoodPosition(){
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
+}
